b644a2b9c77f52075d0d83f0273308c2550b3d98
trent <122744132+trentbenson08@users.noreply.github.com>
2023-02-17 11:38:04 -0600

    Edit styles.css script.js

--- script.js
+++ script.js
@@ -2,33 +2,74 @@
 
 
 function main(){
-    // make expression object out here
-    const exp = new Expression();
     const button = document.querySelectorAll('.btn');
     const screenVal = document.querySelector('#screenVal');
+    let inputArr =[];
+    let input = '';
+    let dotCount = 0;
 
     button.forEach(btn =>{
         btn.addEventListener('click', (e) =>{
-            const eventInput = e.target.getAttribute('id');
-            exp.addEle(eventInput);
-            screenVal.textContent += eventInput;
-            
+            let ei = e.target.getAttribute('id');
+
+            if(ei === '_clear'){
+                input ='';
+                inputArr =[];
+                screenVal.textContent = input;
+            }
+            else if(!isNaN(ei) && input.length < 8){
+                input += String(ei);
+                screenVal.textContent = input;
+            }
+            else if(ei[0] === '.' && dotCount < 1){
+                input += String(ei);
+                screenVal.textContent = input;
+                dotCount++;
+            }
+            else if(ei[0] === '_' && parseFloat(input)){
+                inputArr.push(parseFloat(input));
+                dotCount = 0;
+                
+                if(ei === '_eval'){
+                    screenVal.textContent = evaluate(inputArr);
+                    inputArr = [];
+                    input = '';
+
+                }
+                else{
+                    inputArr.push(ei);
+                    input = '';
+                    screenVal.textContent = input;
+                }
+            }
+
+            console.log(inputArr);
+
+
 
         })
     })
 }
 
-function Expression(){
-    this.expArr = [];
-
-    this.addEle = (item) =>{
-        this.expArr.push(item)
-        console.log(...this.expArr)
-        
+function evaluate(arr){
+    let total = arr[0];
+    for(let i=1; i< arr.length; i+=2){
+        if(arr[i] === '_add'){
+            total += arr[i+1];
+        }
+        else if(arr[i] === '_subtract'){
+            total -= arr[i+1];
+        }
+        else if(arr[i] === '_multiply'){
+            total *= arr[i+1];
+        }
+        else if(arr[i] === '_divide'){
+            total /= arr[i+1];
+        }
     }
-    this.evaluate = ()=>{
-        console.log('evaluate me please')
-    }
+    return total
 }
+
+//evaluate()
 
 main()
